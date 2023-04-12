cc3485d08a2bbf376dc0a13cc8b2af89e472a998
nvaccina <niccolovac@gmail.com>
2023-04-12 14:41:30 +0200

    Stampa h1 con messaggio, con data

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
 
+const {createApp} = Vue;
+
+createApp({
+
+  data(){
+
+    return{
+      message: 'Buongiorno Boolean'
+    }
+  }
+}).mount('#app')
